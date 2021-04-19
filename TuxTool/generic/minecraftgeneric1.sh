d2bb483b2c65e02a5dc4be589b89676d748706ab
Q-dev <82529475+QV-dev@users.noreply.github.com>
2021-04-19 10:43:39 -0600

    Fixed flase flag if someone is using Lunar

--- TuxTool/generic/minecraftgeneric1.sh
+++ TuxTool/generic/minecraftgeneric1.sh
@@ -13,10 +13,24 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with TuxTool.  If not, see <https://www.gnu.org/licenses/>.
+
 SystemBoot=$(stat -c '%Y' /dev/null)
 CurrentUser=$(users)
 MCProcessTime=$(stat -c '%Y' /home/$CurrentUser/.minecraft/launcher_log.txt)
-if [[ $SystemBoot > $MCProcessTime ]]; then
-echo -e User has been detected for Generic cheat A >> /tmp/scanresults.txt
+LauncherLog=/home/$CurrentUser/.minecraft/launcher_log.txt
+if [ -e /home/$CurrentUser/.minecraft/versions ]; then
+:
+else
+echo -e User has been detected for generic cheat A >> /tmp/scanresults.txt
 fi
+if [ -e /home/$CurrentUser/.minecraft/launcher_log.txt ]; then
+:
+else 
+echo -e User is using a third party launcher \(Lunar, Badlion, Old MC launcher, etc.\) >> /tmp/scanresults.txt
+fi
+if [[ -e /home/$CurrentUser/.minecraft/launcher_log.txt ]] && [[ $SystemBoot > $MCProcessTime ]]; then
+echo -e User has been detected for generic cheat A >> /tmp/scanresults.txt
+fi
+cat /tmp/scanresults.txt
 # generic to check if the user changed their minecraft directory
+# KAWAII IS COOL
